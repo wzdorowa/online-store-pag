bd9e6ff2455196c026e486f0961b98fc5c3a7c56
wzdorowa <w.zdorowa@gmail.com>
2022-04-21 16:25:56 +0300

    Написала компонент AppSlider.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <AppHeader />
+  <AppSlider
+    v-bind:images="sliderImages"
+  />
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader.vue'
+import AppSlider from '@/components/AppSlider.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      sliderImages: [
+        { id: 0, src: '/slider-images/product-view1.jpeg', productName: 'Пижама для девочек' },
+        { id: 1, src: '/slider-images/product-view2.jpeg', productName: 'Пижама для девочек' },
+        { id: 2, src: '/slider-images/product-view3.jpeg', productName: 'Пижама для девочек' },
+        { id: 3, src: '/slider-images/product-view4.jpeg', productName: 'Пижама для девочек' },
+        { id: 4, src: '/slider-images/product-view5.jpeg', productName: 'Пижама для девочек' },
+      ]
+    }
+  },
   components: {
-    AppHeader
+    AppHeader, AppSlider
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/AppSlider.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="slider">
+    <div class="slider__image-container"
+      v-for="image in images"
+      v-bind:key="image.src"
+    >
+      <img
+        v-if="image.id === currentSlideIndex"
+        v-bind:src="image.src"
+        v-bind:alt="image.productName"
+        v-bind:aria-label="image.productName"
+        class="slider__image">
+    </div>
+    <div class="slider__images-mini">
+      <img class="slider__image-mini"
+      v-for="image in images"
+      v-bind:key="image.src"
+      v-bind:src="image.src"
+      v-bind:alt="image.productName"
+      @click="setActiveSlide(image.id)">
+    </div>
+  </div>
+    
+</template>
+
+<script>
+
+export default {
+  props: ['images'],
+  data() {
+    return {
+      currentSlideIndex: 0,
+    }
+  },
+  methods: {
+    setActiveSlide(id) {
+      this.currentSlideIndex = id;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .slider {
+    position: relative;
+
+    &__image {
+      width: 100%;
+      height: 100%;
+      display: block;
+    }
+
+    &__images-mini {
+      position: absolute;
+      top: 29px;
+      left: 24px;
+    }
+
+    &__image-mini {
+      display: block;
+      width: 40px;
+      height: 52px;
+      margin-bottom: 4px;
+    }
+  }
+</style>
