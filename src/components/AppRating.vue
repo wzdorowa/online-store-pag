155596662b97a48a6c03d9c84622f734fbca6695
wzdorowa <w.zdorowa@gmail.com>
2022-04-21 18:18:39 +0300

    Написала компонент AppRaing

---
+++ src/components/AppRating.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+.rating
+  .rating__item-wrapper(v-for="(item, index) in amountStar")
+    .rating__item.rating__item_active(v-if="index < ratingStar")
+    .rating__item(v-else)
+</template>
+
+<script>
+
+export default {
+  props: ['ratingStar'],
+  data() {
+    return {
+      amountStar: [1, 2, 3, 4, 5],
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .rating {
+    display: flex;
+    width: 58px;
+    justify-content: space-between;
+
+    &__item {
+      width: 10px;
+      height: 10px;
+      background-image: url('@/components/icons/star.svg');
+
+      &_active {
+        background-image: url('@/components/icons/star-active.svg');
+      }
+    }
+  }
+</style>
