e2cc2793edacf934b4117e1ff691b743e97ead6f
wzdorowa <w.zdorowa@gmail.com>
2022-04-28 15:55:29 +0300

    Добавила в проект компонент AppFooter

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,14 @@
       AppProduct(v-bind:product="product")
     .content__gallery
       AppGallery(:images="gallery")
+  AppFooter(v-bind:navigation="navigation")
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader.vue'
 import AppProduct from '@/components/AppProduct.vue'
 import AppGallery from '@/components/AppGallery.vue'
+import AppFooter from '@/components/AppFooter.vue'
 
 export default {
   name: 'App',
@@ -39,16 +41,37 @@ export default {
         sizes: ['80-86', '86-92', '92-98', '98-104', '104-110'],
       },
       gallery: [
-          { id: 1, src: '/gallery/style-1.jpg', name: 'style 1', numberFavorite: 350, href: '/' },
-          { id: 2, src: '/gallery/style-2.jpg', name: 'style 1', numberFavorite: 32, href: '/' },
-          { id: 3, src: '/gallery/style-3.jpg', name: 'style 1', numberFavorite: 146, href: '/' },
-          { id: 4, src: '/gallery/style-4.jpg', name: 'style 1', numberFavorite: 200, href: '/' },
-          { id: 5, src: '/gallery/style-5.jpg', name: 'style 1', numberFavorite: 279, href: '/' },
-        ],
+        { id: 1, src: '/gallery/style-1.jpg', name: 'style 1', numberFavorite: 350, href: '/' },
+        { id: 2, src: '/gallery/style-2.jpg', name: 'style 1', numberFavorite: 32, href: '/' },
+        { id: 3, src: '/gallery/style-3.jpg', name: 'style 1', numberFavorite: 146, href: '/' },
+        { id: 4, src: '/gallery/style-4.jpg', name: 'style 1', numberFavorite: 200, href: '/' },
+        { id: 5, src: '/gallery/style-5.jpg', name: 'style 1', numberFavorite: 279, href: '/' },
+      ],
+      navigation: [
+        {
+          sectionName: 'Покупателям',
+          links: [
+            { name: 'Каталог', href: '/' },
+            { name: 'Акции', href: '/' },
+            { name: 'Бренды', href: '/' }
+          ]
+        },
+        {
+          sectionName: 'О нас',
+          links: [
+            { name: 'О компании', href: '/' },
+            { name: 'Новости', href: '/' },
+            { name: 'Команда', href: '/' }
+          ]
+        }
+      ],
     }
   },
   components: {
-    AppHeader, AppProduct, AppGallery
+    AppHeader,
+    AppProduct,
+    AppGallery,
+    AppFooter
   }
 }
 </script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,121 @@
+<template lang="pug">
+.footer
+  .footer__navigation
+    ul.footer__list(v-for="column in navigation")
+      h2.footer__list-title {{column.sectionName}}
+      li.footer__item-list(v-for="link in column.links")
+        a.footer__item-link(v-bind:href="link.href") {{link.name}}
+  .footer__subscribe
+    h2.footer__subscribe-title Узнайте первыми о новинках и акциях
+    .footer__subscribe-form
+      input.footer__subscribe-input(placeholder="Адрес электронной почты")
+      span.footer__subscribe-cancel
+    button.footer__subscribe-button Подписаться
+</template>
+
+<script>
+export default {
+  props: ['navigation']
+}
+</script>
+
+<style lang="scss">
+  .footer {
+    padding: 20px;
+
+    &__navigation {
+      display: grid;
+      grid-template-columns: repeat(2, 51.2%);
+      margin-top: 23px;
+    }
+
+    &__list {
+      list-style: none;
+      padding: 0;
+    }
+
+    &__list-title {
+      font-size: 17px;
+      font-weight: 400;
+      text-transform: uppercase;
+      margin-bottom: 16px;
+    }
+
+    &__item-list {
+      margin-top: 9px;
+    }
+
+    &__item-link {
+      letter-spacing: 0.04em;
+      text-decoration: none;
+      color: inherit;
+    }
+
+    &__subscribe {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      margin-top: 60px;
+    }
+
+    &__subscribe-title {
+      max-width: 255px;
+      margin-right: auto;
+      font-size: 17px;
+      font-weight: 400;
+      text-transform: uppercase;
+      margin-bottom: 20px;
+    }
+
+    &__subscribe-form {
+      width: 100%;
+      margin-bottom: 26px;
+    }
+
+    &__subscribe-input {
+      width: 100%;
+      padding: 13px 16px;
+      border: none;
+      border-bottom: 1px solid #C4C4C4;
+
+      &::placeholder {
+        font-size: 14px;
+        letter-spacing: 0.07em;
+        color: #828282;
+      }
+    }
+
+    &__subscribe-cancel {
+      position: relative;
+
+      &::before {
+        position: absolute;
+        content: "";
+        width: 11px;
+        right: 10px;
+        top: 10px;
+        border-top: 1px solid #C4C4C4;
+        transform: matrix(-0.71, -0.71, -0.71, 0.71, 0, 0);
+      }
+
+      &::after {
+        position: absolute;
+        content: "";
+        width: 11px;
+        right: 11px;
+        top: 10px;
+        border-top: 1px solid #C4C4C4;
+        transform: rotate(-45deg);
+      }
+    }
+
+    &__subscribe-button {
+      font-size: inherit;
+      color: inherit;
+      letter-spacing: 0.04em;
+      background: #FFFFFF;
+      padding: 8px 28px;
+      border: 1px solid #333333;
+    }
+  }
+</style>
